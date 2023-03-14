33662dfb78310781af90f99070277bfb8e71e1cf
Rod-Vadar <rod643@hotmail.co.uk>
2023-03-14 16:25:49 +0000

    Add items

---
+++ BOOK_FLIGHT/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BookFlightConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'BOOK_FLIGHT'
---
+++ BOOK_FLIGHT/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .models import Item
+
+# Create your views here.
+def get_todo_list(request):
+    items = Item.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request, 'todo_list.html', context)
